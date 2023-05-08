70fa4721d95074e30d95b1a43c42f972adf4c8d3
Rishabh Jaiswal <rraj411@gmail.com>
2023-05-08 23:25:34 +0530

    feature(navbar): Add Navbar and Carousel to Nextjs App

---
+++ components/Navbar.tsx
@@ -0,0 +1,63 @@
+import Link from "next/link";
+
+export const Navbar = () => {
+	return (
+		<>
+			<div className="navbar bg-base-100">
+				<div className="navbar-start">
+					<div className="dropdown">
+						<label tabIndex={0} className="btn btn-ghost lg:hidden">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								className="h-5 w-5"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth="2"
+									d="M4 6h16M4 12h8m-8 6h16"
+								/>
+							</svg>
+						</label>
+						<ul
+							tabIndex={0}
+							className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+						>
+							<li>
+								<Link href="/">About US</Link>
+							</li>
+							<li>
+								<Link href="/">Gallery</Link>
+							</li>
+							<li>
+								<Link href="/">Cart</Link>
+							</li>
+						</ul>
+					</div>
+					<a className="btn btn-ghost normal-case text-xl">
+						Mani Events
+					</a>
+				</div>
+				<div className="navbar-center hidden lg:flex">
+					<ul className="menu menu-horizontal px-1">
+						<li>
+							<a>About US</a>
+						</li>
+						<li>
+							<a>Gallery</a>
+						</li>
+						<li>
+							<a>Cart</a>
+						</li>
+					</ul>
+				</div>
+				<div className="navbar-end">
+					<a className="btn">Get started</a>
+				</div>
+			</div>
+		</>
+	);
+};
