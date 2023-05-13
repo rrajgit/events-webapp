4bbae524953003fd226a7b4566079a9b1e6fa0c9
Sirivella Sandeep <sirivella.ramu26@gmail.com>
2023-05-13 23:46:18 +0530

    Fixed(Navbar) align the nav items to end from middle

--- components/CardModal.tsx
+++ components/CardModal.tsx
@@ -1,36 +1,36 @@
 import Card from "./Card";
 export default function CardModal({ card }) {
-	return (
-		<>
-			{/* The button to open modal */}
+    return (
+        <>
+            {/* The button to open modal */}
 
-			<label
-				htmlFor="my-modal-4"
-				className="block w-80 bg-transparent border-0	border hover:bg-transparent"
-			>
-				<Card card={card} />
-			</label>
+            <label
+                htmlFor="my-modal-4"
+                className="block w-80 bg-transparent border-0	border hover:bg-transparent"
+            >
+                <Card card={card} />
+            </label>
 
-			{/* Put this part before </body> tag */}
-			<input type="checkbox" id="my-modal-4" className="modal-toggle" />
-			<label htmlFor="my-modal-4" className="modal cursor-pointer">
-				<label className="modal-box relative" htmlFor="">
-					<h3 className="text-lg font-bold">
-						Congratulations random Internet user!
-					</h3>
-					<p className="py-4">Photograph</p>
-					<figure className="max-w-lg">
-						<img
-							className="h-auto max-w-full rounded-lg"
-							src={card.image_URL}
-							alt="image description"
-						/>
-						<figcaption className="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">
-							Image caption
-						</figcaption>
-					</figure>
-				</label>
-			</label>
-		</>
-	);
+            {/* Put this part before </body> tag */}
+            <input type="checkbox" id="my-modal-4" className="modal-toggle" />
+            <label htmlFor="my-modal-4" className="modal cursor-pointer">
+                <label className="modal-box relative" htmlFor="">
+                    <h3 className="text-lg font-bold">
+                        Congratulations random Internet user!
+                    </h3>
+                    <p className="py-4">Photograph</p>
+                    <figure className="max-w-lg">
+                        <img
+                            className="h-auto max-w-full rounded-lg"
+                            src={card.image_URL}
+                            alt="image description"
+                        />
+                        <figcaption className="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">
+                            Image caption
+                        </figcaption>
+                    </figure>
+                </label>
+            </label>
+        </>
+    );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,63 +1,63 @@
 import Link from "next/link";
 
 export const Navbar = () => {
-	return (
-		<>
-			<div className="navbar bg-base-100">
-				<div className="navbar-start">
-					<div className="dropdown">
-						<label tabIndex={0} className="btn btn-ghost lg:hidden">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								className="h-5 w-5"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth="2"
-									d="M4 6h16M4 12h8m-8 6h16"
-								/>
-							</svg>
-						</label>
-						<ul
-							tabIndex={0}
-							className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-						>
-							<li>
-								<Link href="/">About US</Link>
-							</li>
-							<li>
-								<Link href="/gallery">Gallery</Link>
-							</li>
-							<li>
-								<Link href="/cart">Cart</Link>
-							</li>
-						</ul>
-					</div>
-					<a className="btn btn-ghost normal-case text-xl">
-						Mani Events
-					</a>
-				</div>
-				<div className="navbar-center hidden lg:flex">
-					<ul className="menu menu-horizontal px-1">
-						<li>
-							<Link href="/">About Us</Link>
-						</li>
-						<li>
-							<Link href="/gallery">Gallery</Link>
-						</li>
-						<li>
-							<Link href="/cart">Cart</Link>
-						</li>
-					</ul>
-				</div>
-				<div className="navbar-end">
-					<a className="btn">Get started</a>
-				</div>
-			</div>
-		</>
-	);
+    return (
+        <>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16"
+                                />
+                            </svg>
+                        </label>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+                        >
+                            <li>
+                                <Link href="/">About US</Link>
+                            </li>
+                            <li>
+                                <Link href="/gallery">Gallery</Link>
+                            </li>
+                            <li>
+                                <Link href="/cart">Cart</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <Link
+                        href="/"
+                        className="btn btn-ghost normal-case text-xl"
+                    >
+                        Mani Events
+                    </Link>
+                </div>
+                <div className="navbar-end hidden lg:flex">
+                    <ul className="menu menu-horizontal px-3">
+                        <li className="mx-14">
+                            <Link href="/">About Us</Link>
+                        </li>
+                        <li className="mx-14">
+                            <Link href="/gallery">Gallery</Link>
+                        </li>
+                        <li className="mx-14">
+                            <Link href="/cart">Cart</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
 };
